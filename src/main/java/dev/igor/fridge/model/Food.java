4886cd24799d6d2c0f00ccb5785d2d6420055dcf
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2025-12-19 19:32:42 -0300

    Implementação do model/entidade food

--- src/main/java/dev/igor/fridge/model/Food.java
+++ src/main/java/dev/igor/fridge/model/Food.java
@@ -1,5 +1,25 @@
 package dev.igor.fridge.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate expirationDate;
+    private Integer quantity;
 
 }
